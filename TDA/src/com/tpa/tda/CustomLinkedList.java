b9756abee450bed9141751b0cf51feafab501cd4
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2019-05-03 21:10:36 -0300

    Add partial LinkedList and tests.

---
+++ TDA/src/com/tpa/tda/CustomLinkedList.java
@@ -0,0 +1,178 @@
+package com.tpa.tda;
+
+public class CustomLinkedList<T> {
+
+	private Node<T> firstElement;
+	private int count;
+
+	public CustomLinkedList() {
+		this.count = 0;
+	}
+
+	/**
+	 * Insert element at the end of the list
+	 * 
+	 * @param value
+	 */
+	public void pushBack(T value) {
+		Node<T> newElement = new Node<T>(value);
+		if (firstElement == null) {
+			firstElement = newElement;
+		} else {
+			Node<T> aux = firstElement;
+			while (aux.next != null) {
+				aux = aux.next;
+			}
+			aux.next = newElement;
+		}
+		count++;
+	}
+
+	/**
+	 * Returns the last element
+	 * 
+	 * @return
+	 */
+	public T popBack() {
+		if (firstElement != null) {
+			T aux;
+			Node<T> current = firstElement;
+			Node<T> previous = null;
+			while (current.next != null) {
+				previous = current;
+				current = current.next;
+			}
+			aux = current.value;
+
+			if (firstElement == current) {
+				firstElement = null;
+			} else {
+				previous.next = null;
+			}
+			count--;
+
+			return aux;
+		}
+
+		return null;
+
+	}
+
+	/**
+	 * Insert element at the first position
+	 * 
+	 * @param value
+	 */
+	public void pushFront(T value) {
+		Node<T> newElement = new Node<T>(value);
+		if (firstElement != null) {
+			newElement.next = firstElement;
+		}
+		firstElement = newElement;
+		count++;
+	}
+
+	/**
+	 * Returns the first element of the list
+	 * 
+	 * @return T
+	 */
+	public T popFront() {
+		T aux;
+		if (firstElement != null) {
+			aux = firstElement.value;
+			firstElement = firstElement.next;
+			count--;
+			return aux;
+		}
+
+		return null;
+	}
+
+	/**
+	 * Remove the first match for value
+	 * 
+	 * @param value
+	 */
+	public void remove(T value) {
+		Node<T> current = firstElement;
+		Node<T> previous = null;
+		while (current != null && current.value != value) {
+			previous = current;
+			current = current.next;
+		}
+
+		if (current == firstElement) {
+			firstElement = null;
+		} else {
+			previous.next = current.next;
+		}
+		count--;
+	}
+
+	public void reverse() {
+		// Da vuelta la lista
+	}
+
+	public void insertAt(int position, T value) {
+		// Inserta valor en posición;
+	}
+
+	public void eraseAt(int position) {
+		// Borra lo que hay en posición
+	}
+
+	public boolean isEmpty() {
+		return firstElement == null;
+	}
+
+	public void empty() {
+		this.firstElement = null;
+	}
+
+	/**
+	 * If exists, returns the position of the given value
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public int search(T value) {
+		int position = 0;
+		Node<T> aux = firstElement;
+
+		while (aux != null && aux.value != value) {
+			aux = aux.next;
+			position++;
+		}
+
+		if (aux != null) {
+			return position;
+		}
+
+		return -1;
+	}
+
+	/**
+	 * Returns value at position
+	 * 
+	 * @param position
+	 * @return
+	 */
+	public T searchAt(int position) {
+		if (position < count) {
+
+		}
+
+		return null;
+	}
+
+}
+
+class Node<T> {
+	T value;
+	Node<T> next;
+
+	public Node(T value) {
+		this.value = value;
+	}
+}
---
+++ TDA/tests/com/tda/tests/LinkedListTests.java
@@ -0,0 +1,144 @@
+package com.tda.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tpa.tda.CustomLinkedList;
+
+public class LinkedListTests {
+
+	CustomLinkedList<Integer> list;
+
+	@Before
+	public void before() {
+		list = new CustomLinkedList<>();
+	}
+
+	@Test
+	public void shouldBeEmptyAtStart() {
+		Assert.assertEquals(true, list.isEmpty());
+	}
+
+	@Test
+	public void shouldNotBeEmptyIfWeAddAnElement() {
+		list.pushBack(3);
+		Assert.assertEquals(false, list.isEmpty());
+	}
+
+	@Test
+	public void shouldGetTheLastInsertedValue() {
+		list.pushBack(3);
+		Assert.assertEquals((Integer) 3, list.popBack());
+	}
+
+	@Test
+	public void shouldGetTheLastInsertedValueAfterAddMultipleValues() {
+		list.pushBack(3);
+		list.pushBack(4);
+		list.pushBack(5);
+		list.pushBack(6);
+		Assert.assertEquals((Integer) 6, list.popBack());
+	}
+
+	@Test
+	public void shouldLetTheListEmptyAfterPopTheOnlyElement() {
+		list.pushBack(3);
+		list.popBack();
+		Assert.assertEquals(true, list.isEmpty());
+	}
+
+	@Test
+	public void shouldGetAllTheInsertedValuesAndLetTheListEmpty() {
+		list.pushBack(3);
+		list.pushBack(4);
+		list.pushBack(5);
+		list.pushBack(6);
+		Assert.assertEquals((Integer) 6, list.popBack());
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals((Integer) 4, list.popBack());
+		Assert.assertEquals((Integer) 3, list.popBack());
+		Assert.assertEquals(true, list.isEmpty());
+	}
+
+	@Test
+	public void shouldInsertElementAtFirstPostion() {
+		list.pushFront(3);
+		Assert.assertEquals((Integer) 3, list.popFront());
+	}
+
+	@Test
+	public void shouldGetAllElementFromStartToEndAndLetTheListEmpty() {
+		list.pushFront(3);
+		list.pushFront(2);
+		list.pushFront(1);
+		Assert.assertEquals((Integer) 1, list.popFront());
+		Assert.assertEquals((Integer) 2, list.popFront());
+		Assert.assertEquals((Integer) 3, list.popFront());
+		Assert.assertEquals(true, list.isEmpty());
+	}
+
+	@Test
+	public void shouldRemove() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.pushBack(3);
+		list.pushBack(4);
+		list.pushBack(5);
+		list.pushBack(3);
+		list.remove(3);
+		Assert.assertEquals((Integer) 3, list.popBack());
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals((Integer) 4, list.popBack());
+		Assert.assertEquals((Integer) 2, list.popBack());
+		Assert.assertEquals((Integer) 1, list.popBack());
+	}
+
+	@Test
+	public void shouldRemoveMultiple() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.pushBack(3);
+		list.pushBack(4);
+		list.pushBack(5);
+		list.pushBack(3);
+		list.remove(3);
+		list.remove(3);
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals((Integer) 4, list.popBack());
+		Assert.assertEquals((Integer) 2, list.popBack());
+		Assert.assertEquals((Integer) 1, list.popBack());
+	}
+
+	@Test
+	public void shouldReturnDefaultValueIfThereIsNoMatch() {
+		Assert.assertEquals(-1, list.search(10));
+	}
+
+	@Test
+	public void shouldReturnPositionIfItHasJustOneElement() {
+		list.pushBack(10);
+		Assert.assertEquals(0, list.search(10));
+	}
+
+	@Test
+	public void shouldReturnPositionIfItHasMoreThanOneElement() {
+		list.pushBack(10);
+		list.pushBack(18);
+		list.pushBack(7);
+		list.pushBack(9);
+		list.pushBack(11);
+		Assert.assertEquals(2, list.search(7));
+	}
+
+	@Test
+	public void shouldReturnFirstPositionFoundIfItHasMultipleRepetedElements() {
+		list.pushBack(12);
+		list.pushBack(18);
+		list.pushBack(7);
+		list.pushBack(9);
+		list.pushBack(11);
+		list.pushBack(9);
+		Assert.assertEquals(3, list.search(9));
+	}
+}
