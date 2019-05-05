1fc90fc994565a71e8d80157d4d1dcba9f18d65a
Alejandro Imperatori <alejandroimperatori@gmail.com>
2019-05-05 19:21:35 -0300

    Added test for pop multiple elements

--- TDA/tests/com/tda/tests/QueueTest.java
+++ TDA/tests/com/tda/tests/QueueTest.java
@@ -39,6 +39,23 @@ public class QueueTest {
 	}
 
 	@Test
+	public void shouldPopMultipleElementsFromQueue() {
+		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
+		myQueue.push(1);
+		myQueue.push(2);
+		myQueue.push(3);
+		myQueue.push(4);
+		Integer value1 = myQueue.pop();
+		Assert.assertEquals(1, value1.intValue());
+		Integer value2 = myQueue.pop();
+		Assert.assertEquals(2, value2.intValue());
+		Integer value3 = myQueue.pop();
+		Assert.assertEquals(3, value3.intValue());
+		Integer value4 = myQueue.pop();
+		Assert.assertEquals(4, value4.intValue());
+	}
+
+	@Test
 	public void shouldPeekTheFirstElementFromQueue() {
 		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
 		myQueue.push(2);
