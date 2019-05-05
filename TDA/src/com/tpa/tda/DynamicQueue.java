5e24ed3a83132a1ced587b198f74bbe4a9a979c6
Alejandro Imperatori <alejandroimperatori@gmail.com>
2019-05-05 18:33:44 -0300

    Added dynamic queue implementation and unit test

---
+++ TDA/src/com/tpa/tda/DynamicQueue.java
@@ -0,0 +1,62 @@
+package com.tpa.tda;
+
+public class DynamicQueue<T> implements AbstractDataType<T> {
+
+	private Node<T> firstElement;
+	private Node<T> lastElement;
+	private int size;
+
+    public DynamicQueue() {
+		firstElement = null;
+		lastElement = null;
+		size = 0;
+    }
+
+	@Override
+	public boolean push(T value) {
+    	
+    	if(firstElement == null)
+    		firstElement = lastElement = new Node<T>(value);
+		else {
+		    Node<T> temp = new Node<T>(value);
+		    lastElement.next = temp;
+		    lastElement = temp;
+		}
+		size++;
+    	
+		return true;
+	}
+
+	@Override
+	public T pop() {
+		if(firstElement == null)
+			return null;
+		T value = firstElement.value;
+		firstElement = firstElement.next;
+		size--;
+		return value;
+	}
+
+	@Override
+	public T peek() {
+		return (firstElement == null) ? null : (T) firstElement.value;
+	}
+
+	@Override
+	public void empty() {
+		firstElement = lastElement = null;
+		size = 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return firstElement == null;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+	
+	
+}
---
+++ TDA/tests/com/tda/tests/QueueTest.java
@@ -0,0 +1,49 @@
+package com.tda.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.tpa.tda.DynamicQueue;
+
+public class QueueTest {
+	
+	@Test
+	public void shouldBeEmptyInitially() {
+		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
+		Assert.assertEquals(true, myQueue.isEmpty());
+	}
+	
+	@Test
+	public void shouldEmpty() {
+		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
+		myQueue.push(2);
+		myQueue.empty();
+		Assert.assertEquals(true, myQueue.isEmpty());
+	}
+	
+	@Test
+	public void shouldPushOneElementToQueue() {
+		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
+		myQueue.push(2);
+		Assert.assertEquals(1, myQueue.size());
+	}
+	
+	@Test
+	public void shouldPopFirstElementFromQueue() {
+		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
+		myQueue.push(2);
+		myQueue.push(3);
+		Integer value = myQueue.pop();
+		Assert.assertEquals(1, myQueue.size());
+		Assert.assertEquals(2, value.intValue());
+	}
+	
+	@Test
+	public void shouldPeekTheFirstElementFromQueue() {
+		DynamicQueue<Integer> myQueue = new DynamicQueue<Integer>();
+		myQueue.push(2);
+		myQueue.push(3);
+		Assert.assertEquals(2, myQueue.peek().intValue());
+	}
+}
+
