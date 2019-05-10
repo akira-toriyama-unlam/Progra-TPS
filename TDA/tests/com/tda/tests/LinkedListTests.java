bc65b077eabfe1df63ad8b8ae01940637c2c349b
Ariel Gonzalez <ariel.damian.gonzalez@gmail.com>
2019-05-09 21:09:20 -0300

    Update tests. Also, add new ones.

--- TDA/tests/com/tda/tests/LinkedListTests.java
+++ TDA/tests/com/tda/tests/LinkedListTests.java
@@ -142,9 +142,152 @@ public class LinkedListTests {
 		Assert.assertEquals(3, list.search(9));
 	}
 	
+	@Test
+	public void shouldDoNothingIfTryToEraseWithNoElements() {
+		list.eraseAt(0);
+		Assert.assertEquals(0, list.size());
+		Assert.assertEquals(true, list.isEmpty());
+	}
 	
 	@Test
-	public void pushBackReverseList() {
+	public void shouldLetTheListEmptyIfEraseTheOnlyElement() {
+		list.pushBack(10);
+		list.eraseAt(0);
+		Assert.assertEquals(0, list.size());
+		Assert.assertEquals(true, list.isEmpty());
+	}
+	
+	@Test
+	public void shouldEraseTheFirstElement() {
+		list.pushBack(10);
+		list.pushBack(5);
+		list.pushBack(3);
+		list.pushBack(1);
+		list.eraseAt(0);
+		
+		Assert.assertEquals((Integer) 1, list.popBack());
+		Assert.assertEquals((Integer) 3, list.popBack());
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals(0, list.size());
+		Assert.assertEquals(true, list.isEmpty());
+	}
+	
+	@Test
+	public void shouldEraseTheLastElement() {
+		list.pushBack(10);
+		list.pushBack(5);
+		list.pushBack(3);
+		list.pushBack(1);
+		list.eraseAt(3);
+		
+		Assert.assertEquals((Integer) 3, list.popBack());
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals((Integer) 10, list.popBack());
+		Assert.assertEquals(0, list.size());
+		Assert.assertEquals(true, list.isEmpty());
+	}
+	
+	@Test
+	public void shouldEraseNodeAtRandomPosition() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.pushBack(3);
+		list.pushBack(4);
+		list.pushBack(5);
+		
+		list.eraseAt(0);
+		list.eraseAt(1);
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals((Integer) 4, list.popBack());
+		Assert.assertEquals((Integer) 2, list.popBack());
+	}
+
+	@Test
+	public void shouldDoNothingIfItsAnInvalidPosition() {
+		list.insertAt(40, 10);
+		list.insertAt(-3, 10);
+		Assert.assertEquals(0, list.size());
+		Assert.assertEquals(true, list.isEmpty());
+	}
+	
+	@Test
+	public void shouldInsertAtPosition0WithEmptyList() {
+		list.insertAt(0, 10);
+		Assert.assertEquals((Integer) 10, list.popBack());
+	}
+	
+	@Test
+	public void shouldInsertAtTheEnd() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.pushBack(3);
+		list.insertAt(3, 4);
+		Assert.assertEquals((Integer) 4, list.popBack());
+		Assert.assertEquals(3, list.size());
+	}
+	
+	@Test
+	public void shouldInsertInTheMiddle() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.pushBack(4);
+		list.pushBack(5);
+		list.insertAt(2, 3);
+		Assert.assertEquals((Integer) 5, list.popBack());
+		Assert.assertEquals((Integer) 4, list.popBack());
+		Assert.assertEquals((Integer) 3, list.popBack());
+		Assert.assertEquals((Integer) 2, list.popBack());
+		Assert.assertEquals((Integer) 1, list.popBack());
+	}
+	
+	@Test
+	public void shouldSearcNodeInFirstAndLastPosition() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.pushBack(3);
+		list.pushBack(4);
+		list.pushBack(5);
+		
+		Assert.assertEquals((Integer)1, list.searchAt(0));
+		Assert.assertEquals((Integer)5, list.searchAt(4));
+		Assert.assertEquals(null, list.searchAt(5));
+	}
+	
+	
+	
+	/////////
+	@Test
+	public void shouldDoNothingIfTryToReverseAnEmptyList() {
+		list.reverse();
+		
+		Assert.assertEquals(true, list.isEmpty());
+		Assert.assertEquals(0, list.size());
+	}
+	
+	@Test
+	public void shouldReverseASingleElement() {
+		list.pushBack(1);
+		list.reverse();
+		
+		Assert.assertEquals(false, list.isEmpty());
+		Assert.assertEquals(1, list.size());
+		Assert.assertEquals((Integer) 1, list.popBack());
+	}
+	
+	@Test
+	public void shouldReverseTwoElements() {
+		list.pushBack(1);
+		list.pushBack(2);
+		list.reverse();
+		
+		Assert.assertEquals(false, list.isEmpty());
+		Assert.assertEquals(2, list.size());
+		Assert.assertEquals((Integer) 1, list.popBack());
+		Assert.assertEquals((Integer) 2, list.popBack());
+	}
+	
+	@Test
+	public void shouldReverseMoreThanTwoElements() {
 		list.pushBack(1);
 		list.pushBack(2);
 		list.pushBack(3);
@@ -158,55 +301,5 @@ public class LinkedListTests {
 		Assert.assertEquals((Integer) 3, list.popBack());
 		Assert.assertEquals((Integer) 4, list.popBack());
 		Assert.assertEquals((Integer) 5, list.popBack());
-	}
-	
-	
-	@Test
-	public void insertNodeAtRandomPosition() {
-		list.pushBack(1);
-		list.pushBack(2);
-		list.pushBack(3);
-		list.pushBack(4);
-		list.pushBack(5);
-		
-		Assert.assertEquals((Integer)2, list.searchAt(1));
-		list.insertAt(0, 0);
-		Assert.assertEquals(2, list.search(2));
-		Assert.assertEquals(0, list.search(0));
-		
-		Assert.assertEquals((Integer) 5, list.popBack());
-		Assert.assertEquals((Integer) 4, list.popBack());
-		Assert.assertEquals((Integer) 3, list.popBack());
-		Assert.assertEquals((Integer) 2, list.popBack());
-		Assert.assertEquals((Integer) 1, list.popBack());
-		Assert.assertEquals((Integer) 0, list.popBack());
-	}
-	
-	@Test
-	public void eraseNodeAtRandomPosition() {
-		list.pushBack(1);
-		list.pushBack(2);
-		list.pushBack(3);
-		list.pushBack(4);
-		list.pushBack(5);
-		
-		list.eraseAt(0);
-		list.eraseAt(1);
-		Assert.assertEquals((Integer) 5, list.popBack());
-		Assert.assertEquals((Integer) 4, list.popBack());
-		Assert.assertEquals((Integer) 2, list.popBack());
-	}
-	
-	@Test
-	public void searcNodeAtRandomPosition() {
-		list.pushBack(1);
-		list.pushBack(2);
-		list.pushBack(3);
-		list.pushBack(4);
-		list.pushBack(5);
-		
-		Assert.assertEquals((Integer)1, list.searchAt(0));
-		Assert.assertEquals((Integer)5, list.searchAt(4));
-		Assert.assertEquals(null, list.searchAt(5));
 	}
 }
